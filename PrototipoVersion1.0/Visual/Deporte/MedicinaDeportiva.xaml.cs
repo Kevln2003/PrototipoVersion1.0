511abd6f9efc6073b1d0b0bcd403bc7dce109cf7
KevinKeyPR <k3vin.h3rnand3zceped@gmail.com>
2025-01-11 20:33:21 -0600

    Add project files.

---
+++ PrototipoVersion1.0/Visual/Deporte/MedicinaDeportiva.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using PrototipoVersion1._0.Visual.Plantilla;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PrototipoVersion1._0.Visual.Deporte
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MedicinaDeportiva : Page
+    {
+        public MedicinaDeportiva()
+        {
+            this.InitializeComponent();
+            SideMenu.NavigationRequested += OnNavigationRequested;
+        }
+
+        private void OnNavigationRequested(object sender, string pageType)
+        {
+            if (pageType == "Ingreso")
+            {
+                ContentFrame.Navigate(typeof(Ingreso));
+            }
+            if (pageType == "Factura")
+            {
+                ContentFrame.Navigate(typeof(Factura));
+            }
+            if (pageType == "Receta")
+            {
+                ContentFrame.Navigate(typeof(Receta));
+            }
+            if(pageType == "Busqueda")
+            {
+                ContentFrame.Navigate(typeof(Busqueda));
+            }
+
+
+        }
+    }
+}
---
+++ PrototipoVersion1.0/Visual/Plantilla/Opciones.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PrototipoVersion1._0.Visual.Plantilla
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Plantilla : Page
+    {
+        public event EventHandler<string> NavigationRequested;
+
+        public Plantilla()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationRequested?.Invoke(this, "Ingreso");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationRequested?.Invoke(this, "Factura");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) => NavigationRequested?.Invoke(this, "Receta");
+
+        private void Button_Click_3(object sender, RoutedEventArgs e) => NavigationRequested?.Invoke(this, "Busqueda");
+    }
+}
